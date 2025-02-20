099c25be89ef8ab2ff03a92085b3b189b8e46401
tuananh0333 <tuananh0333@gmail.com>
2025-02-20 10:34:47 +0700

    skip button

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,16 @@ import {useEffect, useState} from "react";
 import classNames from "classnames";
 
 const steps: Step[] = [
-  {type: 'do', time: 60},
-  {type: 'rest', time: 5},
-  {type: 'do', time: 5},
+  {type: 'do', time: 30, description: 'push up'},
+  {type: 'rest', time: 50},
+  {type: 'do', time: 100},
+  {type: 'do', time: 125, description: 'squat'},
+  {type: 'rest', time: 30000}
 ]
 
 function App() {
   const [currentStepIndex, setCurrentStepIndex] = useState(0);
-  const [currentStep, setCurrentStep] = useState<Step|null>();
+  const [currentStep, setCurrentStep] = useState<Step | null>();
   const [workoutEnded, setWorkoutEnded] = useState(false);
   const timedOut = () => {
     // temporary set to reset step timeout
@@ -26,16 +28,26 @@ function App() {
     setCurrentStep(steps[currentStepIndex]);
   }, [currentStepIndex]);
 
+  const onSkip = () => {
+    timedOut();
+  }
+
   if (!currentStep) return null;
   return (
     <div className={classNames("w-full h-full flex justify-center items-center text-center transition-all", {
       'bg-neutral-900 text-gray-300': workoutEnded || currentStep.type === 'rest',
       'bg-gray-300 text-neutral-900': !workoutEnded && currentStep.type === 'do',
     })}>
-      {workoutEnded ? <section className={'text-5xl'}>ended</section> : <section>
-        <span className={'text-5xl'} >{currentStep.type}</span>
-        <CountDown time={currentStep.time} timedOut={() => timedOut()}/>
-      </section>}
+      {workoutEnded ? <section className={'text-5xl'}>ended</section> :
+        <>
+          <section>
+            <span className={'text-5xl'}>{currentStep.description ?? currentStep.type}</span>
+            <CountDown time={currentStep.time} timedOut={() => timedOut()}/>
+            <button className={'p-5 w-xs rounded-2xl border-2 border-rose-600 text-rose-600 text-xl hover:bg-rose-800 hover:text-gray-300 active:bg-rose-950'}
+                    onClick={onSkip}
+            >skip</button>
+          </section>
+        </>}
     </div>
   )
 }
