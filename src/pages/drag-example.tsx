3d4d3e98b3fa16e26a8b4743c63bdf604a582666
tuananh0333 <tuananh0333@gmail.com>
2025-03-03 16:25:59 +0700

    drag example

--- src/pages/drag-example.tsx
+++ src/pages/drag-example.tsx
@@ -1,10 +1,11 @@
-import {FC, useEffect, useRef, useState} from "react";
 import * as React from "react";
+import {FC, useCallback, useRef, useState} from "react";
 
 interface Position {
   x: number;
   y: number;
 }
+
 interface DraggableComponentProps {
   defaultPosition?: Position;
   children?: React.ReactNode;
@@ -12,56 +13,53 @@ interface DraggableComponentProps {
 
 const DraggableComponent: FC<DraggableComponentProps> = ({defaultPosition, children}) => {
   const componentRef = useRef(null);
-  const moving = useRef(false);
+  const [moving, setMoving] = useState(false);
   const [x, setX] = useState(defaultPosition?.x ?? 0);
   const [y, setY] = useState(defaultPosition?.y ?? 0);
 
-  useEffect(() => {
-    if (componentRef.current) {
+  const mouseMove = useCallback((event: React.MouseEvent<HTMLDivElement>) => {
+    if (moving && componentRef.current) {
       const element = componentRef.current as HTMLDivElement;
-      const mouseDown = () => moving.current = true;
-      const mouseUp = () => moving.current = false;
-      const mouseMove = (event: MouseEvent) => {
-        if (moving.current && componentRef.current) {
-          const element = componentRef.current as HTMLDivElement;
-          setX(element.offsetLeft + event.movementX);
-          setY(element.offsetTop + event.movementY)
-        }
-      }
-      const mouseLeave = () => moving.current = false
-      element.addEventListener('mousedown', mouseDown);
-      element.addEventListener('mouseup', mouseUp);
-      element.addEventListener('mouseleave', mouseLeave)
-      element.addEventListener('mousemove', mouseMove);
-      return () => {
-        element.removeEventListener('mousedown', mouseDown, true)
-        element.removeEventListener('mouseup', mouseUp, true)
-        element.removeEventListener('mouseleave', mouseLeave, true)
-        element.removeEventListener('mousemove', mouseMove, true)
-      }
+      setX(element.offsetLeft + event.movementX);
+      setY(element.offsetTop + event.movementY)
     }
-  }, []);
+  }, [moving]);
+
+  const mouseDown = () => setMoving(true);
+  const mouseUp = () => setMoving(false);
+  const mouseLeave = () => setMoving(false);
 
   return <div className={'absolute'}
               style={{top: y, left: x, zIndex: moving ? 1000 : 1}}
-              ref={componentRef}>
+              ref={componentRef}
+              onMouseDown={mouseDown}
+              onMouseLeave={mouseLeave}
+              onMouseUp={mouseUp}
+              onMouseMove={(event) => mouseMove(event)}>
     {children}
   </div>
 }
 
 const DragExample: FC = () => {
-  const parentRef = useRef(null);
-  const [toggle, setToggle] = useState(true)
 
-  return <div className={'w-full h-full p-3 relative select-none'} ref={parentRef} >
-    <DraggableComponent defaultPosition={{x: 10, y: 10}}>
-      <div className={'p-10 rounded bg-blue-300'}>hello</div>
-    </DraggableComponent>
-    {toggle && <DraggableComponent defaultPosition={{x: 10, y: 150}}>
-        <div className={'p-3'}>kkk</div>
-    </DraggableComponent>}
+  const [positionList, setPositionList] = useState<Position[]>([]);
 
-    <button onClick={() => setToggle(!toggle)}>reset</button>
+  const add = () => {
+    setPositionList([...positionList, {
+      x: 12,
+      y: positionList.length * 60,
+    }]);
+  }
+
+  return <div className={'w-full h-full p-3 relative select-none'}>
+    <div>
+      <button className={'p-3 bg-blue-300'} onClick={add}>add</button>
+    </div>
+    <div className={'relative'}>
+      {positionList.map((position, index) => (<DraggableComponent defaultPosition={position} key={index}>
+        <div className={'p-4 bg-gray-300'}>item {index}</div>
+      </DraggableComponent>))}
+    </div>
   </div>
 }
 
